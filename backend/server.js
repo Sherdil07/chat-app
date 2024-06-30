44c4584e3d76ae6d7d55292f05a8dcf1982685b8
Sherdil07 <119352761+Sherdil07@users.noreply.github.com>
2024-07-01 00:19:30 +0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const chats = require("./data/data");
+const app = express();
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("home page of the chat app");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  //   console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+app.listen(PORT, () => console.log(`The server is running on port${PORT}`));
